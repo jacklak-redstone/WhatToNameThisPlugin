9e075642455e25218af519f692fc572f29d1db86
jacklak-redstone <176782468+jacklaktimeishere@users.noreply.github.com>
2026-02-21 18:23:06 -0500

    optimiziation

--- src/main/java/org/jacklak_imated_jobfrfr/oresoft/LootItem.java
+++ src/main/java/org/jacklak_imated_jobfrfr/oresoft/LootItem.java
@@ -9,6 +9,16 @@ public class LootItem {
     private final Material item;
     private final int maxAmount;
     private final float percentRarity;
+    private static final LootItem[] ITEMS = new LootItem[] {
+            new LootItem(Material.WIND_CHARGE, 8, 20),
+            new LootItem(Material.FIRE_CHARGE, 3, 15),
+            new LootItem(Material.BAKED_POTATO, 16, 30),
+            new LootItem(Material.STONE_SWORD, 1, 14),
+            new LootItem(Material.IRON_SWORD, 1, 7),
+            new LootItem(Material.STONE_AXE, 1, 9),
+            new LootItem(Material.IRON_AXE, 1, 2),
+            new LootItem(Material.WOODEN_AXE, 1, 18),
+    };
 
     public LootItem(Material i, int max, float percent) {
         item = i;
@@ -17,25 +27,14 @@ public class LootItem {
     }
 
     public static ItemStack chooseItem(Random random) {
-        var items = new LootItem[] {
-                new LootItem(Material.WIND_CHARGE, 8, 20),
-                new LootItem(Material.FIRE_CHARGE, 3, 15),
-                new LootItem(Material.BAKED_POTATO, 16, 30),
-                new LootItem(Material.STONE_SWORD, 1, 14),
-                new LootItem(Material.IRON_SWORD, 1, 7),
-                new LootItem(Material.STONE_AXE, 1, 9),
-                new LootItem(Material.IRON_AXE, 1, 2),
-                new LootItem(Material.WOODEN_AXE, 1, 18),
-        };
-
         var total = 0f;
-        for (var loot : items)
+        for (var loot : ITEMS)
             total += loot.percentRarity;
 
         var rand = random.nextFloat() * total;
 
-        LootItem picked = items[items.length - 1];
-        for (var loot : items) {
+        LootItem picked = ITEMS[ITEMS.length - 1];
+        for (var loot : ITEMS) {
             rand -= loot.percentRarity;
             if (rand <= 0) {
                 picked = loot;
